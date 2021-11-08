39200220083f3a205a065e4e516741b58f2c95aa
lubaek <60066060+lubaek@users.noreply.github.com>
2021-11-08 23:02:10 +0600

    init project

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def homepage(request):
+    return HttpResponse('hello world')
+
+
+def test(request):
+    return render(request, 'test.html')
