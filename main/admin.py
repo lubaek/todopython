b3e5f3c69cc1aa9b0270c4d8f73997ea85fccf04
lubaek <60066060+lubaek@users.noreply.github.com>
2021-11-09 14:24:38 +0600

    added todo and tomeet models to admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Todo, ToMeet
 
 # Register your models here.
+
+admin.site.register(Todo)
+admin.site.register(ToMeet)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.expressions import F
 
 # Create your models here.
+class Todo(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=100)
+    date_of_meeting = models.DateField(auto_now_add=False)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
