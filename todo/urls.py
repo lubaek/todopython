b3fc182e86987fce9347013c3cb653f60e182b36
lubaek <60066060+lubaek@users.noreply.github.com>
2021-11-12 00:01:20 +0600

    added mark and unmark functionality

--- todo/urls.py
+++ todo/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     path("add-tomeet/", add_tomeet, name="add-tomeet"),
     path("habits/", habits),
     path("add-habit/", add_habit, name="add-habit"),
-    path("delete-todo/<id>/", delete_todo, name="delete-todo")
+    path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("unmark-todo/<id>/", unmark_todo, name="unmark-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
